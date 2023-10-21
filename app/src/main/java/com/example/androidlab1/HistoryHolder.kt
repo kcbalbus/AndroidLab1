0fa489373b3a2e70df71b7745bc8d1efcbc729b5
kcbalbus <kc.balbus@gmail.com>
2023-10-21 05:47:52 +0200

    some files i forgot to add

---
+++ app/src/main/java/com/example/androidlab1/HistoryHolder.kt
@@ -0,0 +1,45 @@
+package com.example.androidlab1
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class HistoryHolder (private val resultHistory: ArrayList<Result>): RecyclerView.Adapter<HistoryHolder.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // inflates the card_view_design view
+        // that is used to hold list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.history_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val result:Result = resultHistory[position]
+
+        // sets the image to the imageview from our itemHolder class
+        holder.textViewHisRes.setText(result.BMI.toString())
+
+        // sets the text to the textview from our itemHolder class
+        holder.textViewDate.setText(result.timestamp.toString())
+
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return resultHistory.size
+    }
+
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val textViewDate: TextView = itemView.findViewById(R.id.textViewDate)
+        val textViewHisRes: TextView = itemView.findViewById(R.id.textViewHistoryResult)
+    }
+}
